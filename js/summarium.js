058c2e30a677e23d9ee455ef4199341f67544002
Rafael Schwemmer <rafael.schwemmer@gmail.com>
2012-07-14 11:43:52 +0200

    Bugfix JS

--- js/summarium.js
+++ js/summarium.js
@@ -5,13 +5,13 @@
 $(document).ready(function() {
 	$('#login-button').click(function(e) {
 		$('#login-modal').modal();
-		$('h3').html($(this).text());
+		$('.modal-header h3').html($(this).text());
 		e.preventDefault();
 		$('#login-form-container').load('auth/login', function() {
 			$('a', this).click(function(e) {
 				e.preventDefault();
 				$('#login-form-container').load($(this).attr('href'));				
-				$('h3').html($(this).text());				
+				$('.modal-header h3').html($(this).text());				
 			})
 		});
 	});
