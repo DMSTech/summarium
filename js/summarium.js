a221a4a3a1b0cad95c5b270b564de6c178949678
Rafael Schwemmer <rafael.schwemmer@gmail.com>
2012-07-18 14:43:23 +0200

    Complete Login / Validation / Log out process handled with Ajax in modal dialog

--- js/summarium.js
+++ js/summarium.js
@@ -3,25 +3,61 @@
 */
 
 $(document).ready(function() {
-	$('#login-button').click(function(e) {
-		$('#login-modal').modal();
-		$('.modal-header h3').html($(this).text());
-		e.preventDefault();
-		$('#login-form-container').load('auth/login', function() {
-			$('a', this).click(function(e) {
-				e.preventDefault();
-				$('#login-form-container').load($(this).attr('href'));				
-				$('.modal-header h3').html($(this).text());				
-			})
-		});
-	});
-	
-	$('#register-button').click(function(e) {
-		e.preventDefault();
-		$('#register-modal').modal();
-		$('#register-form-container').load('auth/register');
-	});
-	
+    $('#login-button').click(function(e) {
+        $('#login-modal').modal();
+        $('.modal-header h3').html($(this).text());
+        e.preventDefault();
+        $('#login-form-container').load('auth/login', function() {
+            $('a', this).click(function(e) {
+                e.preventDefault();
+                $('#login-form-container').load($(this).attr('href'));              
+                $('.modal-header h3').html($(this).text());             
+            })
+            $('#login_form').submit(function(e) {
+                e.preventDefault();
+                ajaxLoadLoginForm($(this).attr('action'));
+            });
+            
+        });
+    });
+
+
+    $('#register-button').click(function(e) {
+        e.preventDefault();
+        $('#register-modal').modal();
+        $('#register-form-container').load('auth/register');
+    });
+    
+    // Logout: load logout method of auth controller and then reload page
+    $('#logout-button').click(function(e) {
+        e.preventDefault();
+        $('#login-form-container').load('auth/logout', function() {
+            location.reload();
+        });
+    });
+    
+    function ajaxLoadLoginForm(url) {
+        var dataString = $('#login_form').serialize();
+        $.ajax({
+          type: 'POST',
+          url: url,
+          data: dataString,
+          success: function(data, textStatus, jqXHR) {
+              if (data == 'Login successful!') {
+                  location.reload();
+              }
+              else {
+                  $('#login-form-container').html(data);
+                  $('#login_form').submit(function(e) {
+                    e.preventDefault();
+                      ajaxLoadLoginForm($(this).attr('action'));
+                  });
+              }
+          },
+          dataType: 'html'
+        }); 
+        return false;
+    }   
 });
 
 
