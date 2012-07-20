5acbbdb455b242db5f47e2b1644fe7f4c65d5de8
Rafael Schwemmer <rafael.schwemmer@gmail.com>
2012-07-20 02:37:32 +0200

    Logo file path fix / smaller logo

--- application/views/partials/_footer.php
+++ application/views/partials/_footer.php
@@ -2,6 +2,6 @@
 <footer>
   <p>&copy; 2012 <a href="http://www.textandbytes.com" target="_blank">text &amp; bytes GmbH</a> | 
 	  <a href="http://www-sul.stanford.edu/">Stanford University Libraries and Academic Information resources</a>
-	  <a href="http://www.textandbytes.com"><img class="footer_logo" src="img/text_n_bytes_logo.png" alt="text &amp; bytes logo" /></a>
+	  <a href="http://www.textandbytes.com"><img class="footer_logo" src="<?php echo site_url(); ?>img/text_n_bytes_logo.png" alt="text &amp; bytes logo" height="25" /></a>
   </p>
 </footer>
